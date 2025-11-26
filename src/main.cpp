85e3133701e088ca90e54478a3afbd9e5c0d4b92
Dogukan-Bayhan <dogukanbayhan@hotmail.com>
2025-11-26 23:53:07 +0300

    ECHO command

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,5 @@
 #include "server/RedisServer.hpp"
 
-#include "protocol/RESPParser.cpp"
-#include "commands/CommandHandler.cpp"
-#include "db/KeyValueStore.cpp"
-#include "server/EventLoop.cpp"
-
 int main() {
     RedisServer server(6379);
     server.start();
